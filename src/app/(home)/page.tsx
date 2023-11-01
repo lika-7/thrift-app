218404f56558d8c261bbcb28dbc4ba5029c1abfa
lika-7 <dhwhdtn93@gmail.com>
2023-11-01 17:13:51 +0900

    3-2 홈 UI 생성

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,9 @@
+import Container from "@/components/Container"
 import getProducts, { ProductsParams } from "../actions/getProducts"
+import EmptyState from "@/components/EmptyState"
+import ProductCard from '@/components/ProductCard';
+import getCurrentUser from "../actions/getCurrentUser";
+import FloatingButton from '@/components/FloatingButton';
 
 interface HomeProps {
   searchParams: ProductsParams
@@ -7,12 +12,34 @@ interface HomeProps {
 export default async function Home({searchParams}: HomeProps) {
 
   const products = await getProducts(searchParams)
-
+  const currentUser = await getCurrentUser()
   console.log('@@@',products)
 
   return (
-    <main>
-      누구나 볼 수 있는 페이지입니다.
-    </main>
+    <Container>
+      {/*Category*/}
+      {
+        products?.data.length === 0 
+          ? <EmptyState /> 
+          : <>
+              <div
+                className='grid grid-cols-4 gap-8 pt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'
+              >
+                {products.data.map((product) => ( //map 함수에 ()=>{} 가 아니라 ()=>() 임
+                  <ProductCard 
+                    key={product.id}
+                    currentUser = {currentUser}
+                    data = {product}
+                  />
+                ))}
+              </div>
+            </>
+      }
+      <FloatingButton 
+        href="/products/upload"
+      >
+        +
+      </FloatingButton>
+    </Container>
   )
 }
