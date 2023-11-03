171863dc445b842cac12752eace4be839c988d0b
lika-7 <dhwhdtn93@gmail.com>
2023-11-03 14:09:00 +0900

    3-6 Favorite Delete Router 추가

--- src/app/api/favorites/[productId]/route.ts
+++ src/app/api/favorites/[productId]/route.ts
@@ -34,3 +34,33 @@ export async function POST(request: Request, { params }: {params: Params}){
 
     return NextResponse.json(user)
 }
+
+export async function DELETE(request: Request, { params }: {params: Params}){
+    console.log('params',params)
+    const currentUser = await getCurrentUser()
+    
+    if(!currentUser){
+        return NextResponse.error()//클라이언트에 오류 응답을 반환
+    }
+
+    const { productId } = params
+
+    if(!productId || typeof productId !=='string'){
+        throw new Error('Invalid ID') // 서버상 에러 발생
+    }
+
+    let favoriteIds = [...(currentUser.favoriteIds || [])] // ...쓰는 것은 기존 배열 변경하지 않고 새로운 배열 작성하기 위함
+
+    favoriteIds = favoriteIds.filter(id => id!==productId)
+
+    const user = await prisma?.user.update({
+        where:{
+            id: currentUser.id
+        },
+        data:{
+            favoriteIds: favoriteIds
+        }
+    })
+
+    return NextResponse.json(user)
+}
